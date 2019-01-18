5d07c58911777d5b050c60f22714ea6a54f401ee
CrMarquez <creyesmarquez@gmail.com>
2019-01-17 21:03:13 -0500

    Started Player movement script, added cloud and tree sprites

---
+++ FallenAngel/Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private float speed = 2.5f;
+    public SpriteRenderer m_SpriteRenderer;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+            m_SpriteRenderer.flipX = true;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+            m_SpriteRenderer.flipX = false;
+        }
+
+        //if (Input.GetKey(KeyCode.W))
+        //{
+        //    transform.position += Vector3.up * speed * Time.deltaTime;
+        //}
+        //if (Input.GetKey(KeyCode.S))
+        //{
+        //    transform.position += Vector3.down * speed * Time.deltaTime;
+        //}
+    }
+}
